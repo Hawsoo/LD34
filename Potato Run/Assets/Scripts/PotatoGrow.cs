c7e778ce2643985b6352348c4377cae3adeafcfb
Hawsoo <timoguy99004@gmail.com>
2015-12-12 11:51:30 -0800

    Adds sfx to item

--- Potato Run/Assets/Scripts/PotatoGrow.cs
+++ Potato Run/Assets/Scripts/PotatoGrow.cs
@@ -4,18 +4,26 @@ using System.Collections;
 public class PotatoGrow : MonoBehaviour {
 
     public bool IsLarge = false;
+    private bool IsLargePrev;
     public GameObject PotatoSmall;
     public GameObject PotatoLarge;
+    public AudioClip PowerUp;
 
 	// Use this for initialization
 	void Start () {
-	
+        IsLargePrev = IsLarge;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        PotatoSmall.SetActive(!IsLarge);
-        PotatoLarge.SetActive(IsLarge);
+        if (IsLargePrev != IsLarge)
+        {
+            IsLargePrev = IsLarge;
+            AudioSource.PlayClipAtPoint(PowerUp, Vector3.zero);
+
+            PotatoSmall.SetActive(!IsLarge);
+            PotatoLarge.SetActive(IsLarge);
+        }
 	}
 
     void SetIsLarge(bool isLarge)
