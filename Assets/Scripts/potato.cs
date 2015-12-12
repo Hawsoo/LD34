a0df96d7257c7dde4e037bc809f5e87740605e64
Nash429 <noah.the.buttkickingest@gmail.com>
2015-12-12 14:00:52 -0800

    build 0.1

--- Assets/Scripts/potato.cs
+++ Assets/Scripts/potato.cs
@@ -5,31 +5,21 @@ public class potato : MonoBehaviour {
 
     private Rigidbody2D rb;
     private BoxCollider2D bc;
-    //private Renderer rend;
     public bool hasSecondJumped;
     public bool hasHitFloor;
-    public bool touchingWall;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponentInChildren<BoxCollider2D>();
-        //rend = GetComponent<Renderer>();
         hasSecondJumped = false;
         hasHitFloor = true;
-        touchingWall = false;
 
         rb.AddForce(new Vector2(50.0f, 0.0f));
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-        if(rb.velocity.x<5.0f && !touchingWall)
-        {
-            rb.AddForce(new Vector2(50.0f, 0.0f));
-        }
-
+	void FixedUpdate () {
 
         if (!hasSecondJumped && Input.GetKeyDown("up"))
         {
@@ -41,7 +31,7 @@ public class potato : MonoBehaviour {
         if (hasHitFloor && Input.GetKeyDown("up"))
         {
             //first jump
-            rb.AddForce(new Vector2(0.0f, 150.0f));
+            rb.AddForce(new Vector2(0.0f, 200.0f));
             hasSecondJumped = false;
             hasHitFloor = false;
         }
@@ -52,18 +42,27 @@ public class potato : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D collision)
     {
         //if collision is a floor
-        if (collision.transform.position.y + collision.collider.bounds.size.y / 2.0f < transform.position.y - bc.bounds.size.y / 2.0f)
+        //if (collision.transform.position.y + collision.collider.bounds.size.y / 2.0f < transform.position.y - bc.bounds.size.y / 2.0f)
+        if (collision.gameObject.tag != "wall")
         {
             hasHitFloor = true;
         }
     }
     void OnCollisionStay2D(Collision2D collision)
     {
-        //its a wall
-        if (collision.transform.position.x - collision.collider.bounds.size.x / 2.0f > transform.position.x + bc.bounds.size.x / 2.0f)
+        //if collision is a floor
+        //if (collision.transform.position.y + collision.collider.bounds.size.y / 2.0f < transform.position.y - bc.bounds.size.y / 2.0f)
+        if(collision.gameObject.tag != "wall")
         {
-            touchingWall = true;
+            hasHitFloor = true;
         }
-        else { touchingWall = false; }
+
+        //its a wall
+        //if (collision.transform.position.x - collision.collider.bounds.size.x / 2.0f > transform.position.x + bc.bounds.size.x / 2.0f)
+        if (collision.gameObject.tag == "wall")
+        {
+            rb.AddForce(new Vector2(-60.0f, 0.0f));
+        }
+
     }
 }
