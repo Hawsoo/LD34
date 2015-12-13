ea4922a5cd585db0a53ad0897ae79f43a9fb4875
Hawsoo <timoguy99004@gmail.com>
2015-12-13 00:01:52 -0800

    Fixes discrepencies of certain jump types

---
+++ Assets/FloorCollider.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorCollider : MonoBehaviour {
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        SendMessageUpwards("FloorCollision");
+    }
+}
